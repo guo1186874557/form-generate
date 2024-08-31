72eb6a793933db840d317685487101ada7356da6
一盒十三香 <1186874557@qq.com>
2024-08-31 17:33:38 +0800

    暂存(下班)

---
+++ src/views/code-preview/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import "@vue/repl/style.css";
+
+import { Repl } from "@vue/repl";
+import Monaco from "@vue/repl/monaco-editor";
+// ^ no longer needed after 3.0
+</script>
+
+<template>
+  <Repl :content="123" :show-ts-config="false" :show-import-map="false" :show-compile-output="false" :editor="Monaco" />
+</template>
